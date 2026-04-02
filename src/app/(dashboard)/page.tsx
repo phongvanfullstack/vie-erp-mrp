f99fed90326eba663ee878d94678c3d2afa52d3f
DESKTOP-TV03572\HIEU <phongvanfullstack@gmail.com>
2026-04-02 15:53:45 +0700

    .

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,12 +1,6 @@
-'use client';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 export default function DashboardGroupPage() {
-  const router = useRouter();
-  useEffect(() => {
-    router.replace('/home');
-  }, [router]);
-  return null;
+  redirect('/home');
 }
 
